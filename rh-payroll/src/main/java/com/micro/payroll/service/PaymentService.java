6755bcefffca0af96e46f9612a9ac70a36b1bac5
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-06-23 16:18:50 -0300

    Utilizando o rh-worker no rh-payroll com RestTemplate

--- rh-payroll/src/main/java/com/micro/payroll/service/PaymentService.java
+++ rh-payroll/src/main/java/com/micro/payroll/service/PaymentService.java
@@ -1,13 +1,30 @@
 package com.micro.payroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.micro.payroll.model.Payment;
+import com.micro.payroll.model.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${rh-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bruno Gaudio", 200.0, days);
+		Map<String, String> variavelCarga = new HashMap<>();
+		variavelCarga.put("id", workerId.toString());
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, variavelCarga);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
