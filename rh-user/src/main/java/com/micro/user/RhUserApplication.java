d159b352a69c2afb7a6eb01dbe2ba2575dc6b4b7
Bruno Gaudio de Azevedo Mattos <bruno.mattos@dellavolpe.com.br>
2023-07-14 17:58:28 -0300

    Adicionando o Rh-user, porém necessário corrigir, não está rodando.

---
+++ rh-user/src/main/java/com/micro/user/RhUserApplication.java
@@ -0,0 +1,14 @@
+package com.micro.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RhUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhUserApplication.class, args);
+	}
+}
