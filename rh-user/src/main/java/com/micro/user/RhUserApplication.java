3661c776fca83b49c96ee74db76fb4dd6f2019ff
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-07-17 14:57:14 -0300

    Realizando Seeding do banco e Conhecendo BCrypt Encoder

--- rh-user/src/main/java/com/micro/user/RhUserApplication.java
+++ rh-user/src/main/java/com/micro/user/RhUserApplication.java
@@ -1,14 +1,26 @@
 package com.micro.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class RhUserApplication {
+public class RhUserApplication implements CommandLineRunner{
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RhUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Password Encoded: " + bCryptEncoder.encode("123456"));
+		
 	}
 }
