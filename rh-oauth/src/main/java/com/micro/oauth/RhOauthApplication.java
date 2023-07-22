fdce27c93eef1ff181ccc5c6262c8def820047e2
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-07-21 22:56:31 -0300

    JWT - Classes de Configuração

--- rh-oauth/src/main/java/com/micro/oauth/RhOauthApplication.java
+++ rh-oauth/src/main/java/com/micro/oauth/RhOauthApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@EnableAuthorizationServer
 public class RhOauthApplication {
 
 	public static void main(String[] args) {
