9f59933502caac6fbd3ae29f483100d7c9380bfb
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-08-21 11:56:11 -0300

    Gerando o Token Basic JWT

--- rh-oauth/src/main/java/com/micro/oauth/service/UserService.java
+++ rh-oauth/src/main/java/com/micro/oauth/service/UserService.java
@@ -3,13 +3,16 @@ package com.micro.oauth.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.micro.oauth.feign.UserFeignClient;
 import com.micro.oauth.model.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	@Autowired
 	private UserFeignClient feignClient;
@@ -22,6 +25,16 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found.");
 		}
 		logger.info("Returning email " + email);
+		return usuario;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User usuario = feignClient.findByEmail(username).getBody();
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Email not found.");
+		}
+		logger.info("Returning email " + username);
 		return usuario;
 	}
 }
